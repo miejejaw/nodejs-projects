12188083586f64fc631ddcde05a13f301fa14c2c
mie-jejaw <mieraf.jejaw@a2sv.org>
2024-02-10 18:38:20 +0300

    implement refresh token and logout

--- contact-management-api/src/controllers/authController.js
+++ contact-management-api/src/controllers/authController.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import User from '../models/user.js';
 
 const secretKey = process.env.SECRET_KEY;
- 
+const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
 export const loginUser = async (req, res) => {
   const { username, password } = req.body;
@@ -19,9 +19,32 @@ export const loginUser = async (req, res) => {
       return res.status(401).json({ message: 'Invalid username or password' });
     }
 
-    const token = jwt.sign({ username: user.username, role: user.role }, secretKey);
-    res.json({ token });
+    const accessToken = jwt.sign({ username: user.username, role: user.role }, secretKey, { expiresIn: '15m' });
+    const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret, { expiresIn: '7d' });
+
+    res.json({ accessToken, refreshToken });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
+};
+
+export const refreshAccessToken = (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  if (!refreshToken) {
+    return res.status(401).json({ message: 'Unauthorized: Missing refresh token' });
+  }
+
+  jwt.verify(refreshToken, refreshTokenSecret, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Forbidden: Invalid refresh token' });
+    }
+
+    const accessToken = jwt.sign({ username: user.username, role: user.role }, secretKey, { expiresIn: '15m' });
+    res.json({ accessToken });
+  });
+};
+
+export const logoutUser = (req, res) => {
+  res.clearCookie('refreshToken'); 
+  res.json({ message: 'Logout successful' });
 };
--- contact-management-api/src/routes/authRouter.js
+++ contact-management-api/src/routes/authRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { loginUser } from '../controllers/authController.js';
+import { loginUser, refreshAccessToken, logoutUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
-
+router.post('/refresh-token', refreshAccessToken);
+router.post('/logout', logoutUser);
 export default router;
